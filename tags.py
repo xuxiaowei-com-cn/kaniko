1df596e22d2bb781767e1656484de56e82aff087
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-12-06 14:13:27 +0800

    --ignore-missing

--- tags.py
+++ tags.py
@@ -48,21 +48,21 @@ for tag in resp_json:
             msg += f"docker pull --platform={item} {image} && docker tag {image} $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-{platform} || echo '不存在：{image}'\n"
             msg += f"docker push $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-{platform} || echo '不存在：{image}'\n"
 
-        tool_msg += f"manifest-tool --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD push from-args --platforms {','.join(executor_array)} --template $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH --target $DOCKER_USERNAME/kaniko-project-executor:{tag['name']} || echo '无法合并 $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH'\n"
+        tool_msg += f"manifest-tool --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD push from-args --platforms {','.join(executor_array)} --template $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH --target $DOCKER_USERNAME/kaniko-project-executor:{tag['name']} --ignore-missing || echo '无法合并 $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH'\n"
 
         for item in executor_debug_array:
             platform = item.split('/')[1]
             msg += f"docker pull --platform={item} {image}-debug && docker tag {image}-debug $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-{platform}-debug || echo '不存在：{image}-debug'\n"
             msg += f"docker push $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-{platform}-debug || echo '不存在：{image}-debug'\n"
 
-        tool_msg += f"manifest-tool --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD push from-args --platforms {','.join(executor_debug_array)} --template $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH-debug --target $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-debug || echo '无法合并 $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH-debug'\n"
+        tool_msg += f"manifest-tool --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD push from-args --platforms {','.join(executor_debug_array)} --template $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH-debug --target $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-debug --ignore-missing || echo '无法合并 $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH-debug'\n"
 
         for item in executor_slim_array:
             platform = item.split('/')[1]
             msg += f"docker pull --platform={item} {image}-slim && docker tag {image}-slim $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-{platform}-slim || echo '不存在：{image}-slim'\n"
             msg += f"docker push $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-{platform}-slim || echo '不存在：{image}-slim'\n"
 
-        tool_msg += f"manifest-tool --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD push from-args --platforms {','.join(executor_slim_array)} --template $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH-slim --target $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-slim || echo '无法合并 $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH-slim'\n"
+        tool_msg += f"manifest-tool --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD push from-args --platforms {','.join(executor_slim_array)} --template $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH-slim --target $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-slim --ignore-missing || echo '无法合并 $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH-slim'\n"
 
         print(msg)
         print(tool_msg)
