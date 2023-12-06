65f2d15e781bd47a4b4be88887e4b0bbf2284311
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-12-06 13:44:40 +0800

    增加平台

--- tags.py
+++ tags.py
@@ -23,36 +23,55 @@ file_name = 'tags.sh'
 if os.path.exists(file_name):
     os.remove(file_name)
 
+tool_file_name = 'tool.sh'
+if os.path.exists(tool_file_name):
+    os.remove(tool_file_name)
+
 file = open(file_name, 'w')
+
+tool_file = open(tool_file_name, 'w')
+
+executor_array = ["linux/amd64", "linux/arm64", "linux/s390x", "linux/ppc64le"]
+executor_debug_array = ["linux/amd64", "linux/arm64", "linux/s390x"]
+executor_slim_array = ["linux/amd64", "linux/arm64", "linux/s390x", "linux/ppc64le"]
 
 i = 0
 for tag in resp_json:
     if tag['name'].startswith('v') and tag['name'][1:].replace('.', '').isdigit():
         i = i + 1
         image = f"gcr.io/kaniko-project/executor:{tag['name']}"
-        msg = f"docker pull {image} || echo '不存在：{image}'\n"
-        msg += f"docker pull {image}-debug || echo '不存在：{image}-debug'\n"
-        msg += f"docker pull {image}-slim || echo '不存在：{image}-slim'"
+        msg = ""
+        tool_msg = ""
+
+        for item in executor_array:
+            platform = item.split('/')[1]
+            msg += f"docker pull --platform={item} {image} && docker tag {image} $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-{platform} || echo '不存在：{image}'\n"
+            msg += f"docker push $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-{platform} || echo '不存在：{image}'\n"
+
+        tool_msg += f"manifest-tool --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD push from-args --platforms {','.join(executor_array)} --template $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH --target $DOCKER_USERNAME/kaniko-project-executor:{tag['name']} || echo '无法合并 $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH'\n"
+
+        for item in executor_debug_array:
+            platform = item.split('/')[1]
+            msg += f"docker pull --platform={item} {image}-debug && docker tag {image}-debug $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-{platform}-debug || echo '不存在：{image}-debug'\n"
+            msg += f"docker push $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-{platform}-debug || echo '不存在：{image}-debug'\n"
+
+        tool_msg += f"manifest-tool --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD push from-args --platforms {','.join(executor_debug_array)} --template $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH-debug --target $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-debug || echo '无法合并 $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH-debug'\n"
+
+        for item in executor_slim_array:
+            platform = item.split('/')[1]
+            msg += f"docker pull --platform={item} {image}-slim && docker tag {image}-slim $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-{platform}-slim || echo '不存在：{image}-slim'\n"
+            msg += f"docker push $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-{platform}-slim || echo '不存在：{image}-slim'\n"
+
+        tool_msg += f"manifest-tool --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD push from-args --platforms {','.join(executor_slim_array)} --template $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH-slim --target $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-slim || echo '无法合并 $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-ARCH-slim'\n"
+
         print(msg)
+        print(tool_msg)
         file.write(msg)
+        tool_file.write(tool_msg)
         file.write('\n')
+        tool_file.write('\n')
 
         if num is not None and i >= num:
             break
 
 file.write('\n')
-
-i = 0
-for tag in resp_json:
-    if tag['name'].startswith('v') and tag['name'][1:].replace('.', '').isdigit():
-        i = i + 1
-        image = f"gcr.io/kaniko-project/executor:{tag['name']}"
-        msg = f"docker tag {image} $DOCKER_USERNAME/kaniko-project-executor:{tag['name']} || echo '打标签失败：{image}'\n"
-        msg += f"docker tag {image}-debug $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-debug || echo '打标签失败：{image}-debug'\n"
-        msg += f"docker tag {image}-slim $DOCKER_USERNAME/kaniko-project-executor:{tag['name']}-slim || echo '打标签失败：{image}-slim'"
-        print(msg)
-        file.write(msg)
-        file.write('\n')
-
-        if num is not None and i >= num:
-            break
--- tags_push.sh
+++
@@ -1,7 +0,0 @@
-images=$(docker images | grep $DOCKER_USERNAME/kaniko-project-executor | awk '{print $1":"$2}')
-
-for image in $images
-do
-  echo "准备推送：$image"
-  docker push $image && echo "推送完成：$image"
-done
